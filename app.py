8dd8993d9c9a2cd6dc5af0b5e350bad6f5d07aef
plus4academy <plus4academy2025@gmail.com>
2026-02-06 11:09:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -570,4 +570,5 @@ def server_error(e):
     return render_template('error.html', message="Internal server error"), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
